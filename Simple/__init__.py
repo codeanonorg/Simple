e8a2143aad09eca6bd86d3635e79668ae366e30c
Nathan Graule <solarliner@gmail.com>
2021-01-24 01:52:33 +0100

    feat: add command line parsing and logging

--- Simple/__init__.py
+++ Simple/__init__.py
@@ -5,8 +5,42 @@
  https://opensource.org/licenses/MIT
 """
 
+import logging
+import sys
 from typing import List
+from pathlib import Path
+from argparse import ArgumentParser
+from dataclasses import dataclass
+
+
+@dataclass
+class Options:
+    input: Path
+    output: Path
+    log_level: int
+
+
+def parse_args(args: List[str]) -> Options:
+    parser = ArgumentParser()
+    parser.add_argument("input", nargs=1, type=Path, help="Input HTML file to process")
+    parser.add_argument("-o", "--output", nargs=1, type=Path, help="Output file path")
+    parser.add_argument(
+        "-v", action="count", default=0, help="Increase verbosity level"
+    )
+    args = parser.parse_args(args)
+    return Options(args.input, args.output, logging.WARN - args.v * 10)
+
+
+def create_logger(log_level: int):
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    logger.addHandler(logging.StreamHandler(sys.stderr))
+
 
 def main(args: List[str]) -> int:
-    print("Hello, world")
+    opts = parse_args(args[1:])
+    create_logger(opts.log_level)
+    logger = logging.getLogger(__name__)
+    logger.info(f"Options: {opts}")
+
     return 0
