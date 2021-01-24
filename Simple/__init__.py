213efcf545790e42ee42e2ddc9f66e3626b3200c
Nathan Graule <solarliner@gmail.com>
2021-01-24 01:15:47 +0100

    chore(vscode): setup vscode workspace

--- Simple/__init__.py
+++ Simple/__init__.py
@@ -1,3 +1,10 @@
+"""
+ Copyright (c) 2021 SolarLiner, jdrprod, Arxaqapi
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+"""
+
 from typing import List
 
 def main(args: List[str]) -> int:
--- Simple/__main__.py
+++ Simple/__main__.py
@@ -1,3 +1,10 @@
+"""
+ Copyright (c) 2021 SolarLiner, jdrprod, Arxaqapi
+ 
+ This software is released under the MIT License.
+ https://opensource.org/licenses/MIT
+"""
+
 import sys
 from Simple import main
 
