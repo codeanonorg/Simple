f4108180af2fd7f2cb4ce4879e7d34cb3563d66a
Nathan Graule <solarliner@gmail.com>
2021-02-04 17:00:05 +0100

    refactor: factor out rendering steps into private methods

--- Simple/document.py
+++ Simple/document.py
@@ -5,6 +5,7 @@
  https://opensource.org/licenses/MIT
 """
 
+from Simple.logs import DocumentLogAdapter
 import os
 import logging
 import copy
@@ -18,14 +19,20 @@ logger = logging.getLogger(__name__)
 class Document:
     path: Path
     cwd: Path
+    adapter: DocumentLogAdapter
+    parent: Optional["Document"]
     is_component: bool
     name: str
     inputs: List[str]
     components: Dict[str, "Document"]
 
-    def __init__(self, path: os.PathLike[str]) -> None:
+    def __init__(
+        self, path: os.PathLike[str], parent: Optional["Document"] = None
+    ) -> None:
         self.path = Path(path).resolve().absolute()
         self.cwd = self.path.parent
+        self.adapter = DocumentLogAdapter(logger, self)
+        self.parent = parent
 
         with path.open("rt") as f:
             logger.debug(f"Parsing file '{path}'")
@@ -33,7 +40,7 @@ class Document:
 
         tag = next(tags(self.html.children))
         if tag is not None and tag.name == "def":
-            logger.debug("Input is defining a component")
+            self.adapter.debug("Input is defining a component")
             self.is_component = True
             self.name = str(tag.attrs["name"])
             try:
@@ -49,52 +56,61 @@ class Document:
         for tag in self.html.find_all("include"):
             tag.extract()
             src = tag.attrs["src"]
-            component = Document(self.cwd / src)
+            component = Document(self.cwd / src, self)
             assert (
                 component.is_component
             ), f"Included file at '{component.path}' does not define a component"
             self.components[component.name] = component
 
-    def render(self, context: Dict[str, str]) -> BeautifulSoup:
+    def render(
+        self, context: Dict[str, str], children: Iterable[Tag] = []
+    ) -> BeautifulSoup:
         # Getting a working copy of the structure
         html = copy.deepcopy(self.html)
-        # Getting all 'content' tags
-        contents = html.find_all("content")
 
-        # Replacing content tags with variables in context
-        for c in contents:
-            if "prop" not in c.attrs:
-                logger.warn(f"Content tag should have a 'prop' attribute")
-                c.remove()
-            elif c.attrs["prop"] not in context:
-                logger.warn(f"Variable '{c.attrs['prop']}' not defined in context")
-                c.remove()
-            else:
-                logger.info("Ok")
-                c.replace_with(context[c.attrs["prop"]])
-
-        # Inserting components
-        for name, component in self.components.items():
-            tags = html.find_all(name.lower())
-
-            if len(tags) == 0:
-                logger.warn(f"Component '{name}' is unused in document '{self.path}'")
-            else:
-                for tag in tags:
-                    props = dict(tag.attrs)
-                    for s in props.keys():
-                        if s not in component.inputs:
-                            logger.warn(
-                                f"Attribute '{s}' is not used in component '{name}'"
-                            )
-                    child_context = {**context, **props}
-                    tag.replace_with(component.render(child_context))
+        self._replace_props(html, context)
+        self._replace_children(html, children)
+        self._replace_components(html, context)
 
         # removing the "def" part
         if self.is_component:
             html.find("def").replace_with_children()
 
         return html
+
+    def _replace_components(self, html: BeautifulSoup, context: Dict[str, str]):
+        for name, component in self.components.items():
+            tags = html.find_all(name.lower())
+            if len(tags) == 0:
+                self.adapter.warn(f"<{name} /> is unused")
+            else:
+                for tag in tags:
+                    props = dict(tag.attrs)
+                    for s in props.keys():
+                        if s not in component.inputs:
+                            self.adapter.warn(f"Unknown attribute '{s}' in <{name} />")
+                    child_context = {**context, **props}
+                    tag.replace_with(component.render(child_context, tag.children))
+
+    def _replace_children(self, html: BeautifulSoup, children: Iterable[Tag]):
+        if (placeholder := html.find("children")) is not None:
+            placeholder.replace_with(children)
+
+    def _replace_props(self, html: BeautifulSoup, context: Dict[str, str]):
+        for c in html.find_all("content"):
+            if "prop" not in c.attrs:
+                self.adapter.warn(f"Content tag should have a 'prop' attribute")
+                if c.remove is not None:
+                    c.remove()
+            elif c.attrs["prop"] not in context:
+                self.adapter.warn(
+                    f"Variable '{c.attrs['prop']}' not defined in context"
+                )
+                if c.remove is not None:
+                    c.remove()
+            else:
+                self.adapter.debug(f"Replacing reference to {c.attrs['prop']}")
+                c.replace_with(context[c.attrs["prop"]])
 
 
 def tags(it: Iterator[Any]) -> Iterator[Tag]:
